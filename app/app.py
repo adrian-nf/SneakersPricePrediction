8c95b92f7a8afafe2b2f7a8bce08c2a92af54b69
Adrian <145907864+adrian-nf@users.noreply.github.com>
2024-10-30 19:57:08 +0100

    Cambios en Docker

--- app/app.py
+++ app/app.py
@@ -4,8 +4,10 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
+import xgboost
 
 model_path = "./models/model1.pkl"
+scaler_path = "./data/scaler.pkl"
 
 TRAIN_COLS = ['Order Date', 'Retail Price', 'Release Date', 'Shoe Size', 'Order Year', 'Order Month', 'Order Day', 'Order Day of Week', 'Release Year', 'Release Month', 'Release Day', 'Release Day of Week', 'Brand_Adidas', 'Brand_Nike', 'Sub-brand_Air Jordan', 'Sub-brand_Yeezy', 'Product Line_Air Max', 'Product Line_Air Presto', 'Product Line_Air VaporMax', 'Product Line_Blazer Mid', 'Product Line_Boost', 'Product Line_Flyknit', 'Product Line_Mercurial', 'Product Line_Retro', 'Product Line_Zoom Fly', 'Model_1', 'Model_350', 'Model_90', 'Model_97', 'Version_2pt0', 'Version_V2', 'Height_High', 'Height_Low', 'Collaboration_Off White', 'Color_Oxford Tan', 'Color_Eve', 'Color_AF100', 'Color_Hallows', 'Color_White', 'Color_Chicago', 'Color_Elemental', 'Color_Queen', 'Color_Black-White', 'Color_Copper', 'Color_Blue Tint', 'Color_Frozen', 'Color_Black', 'Color_Yellow', 'Color_Menta', 'Color_All', 'Color_Zebra', 'Color_Turtledove', 'Color_Moonrock', 'Color_Semi', 'Color_Total-Orange', 'Color_Air', 'Color_Sesame', 'Color_Pink', 'Color_Pirate Black', 'Color_Green', 'Color_Beluga', 'Color_Wolf Grey', 'Color_Force', 'Color_Grim Reaper', 'Color_University Blue', 'Color_Black-Silver', 'Color_Desert Ore', 'Color_Red', 'Color_Cream White', 'Color_Core', 'Color_Rose', 'Color_Volt']
 
@@ -24,7 +26,7 @@ PRODUCT_LINES = [None, "Boost", "Air Max", "Air Presto", "Air VaporMax", "Air Fo
                  "React Hyperdunk", "Hyperdunk", "Mercurial", "Retro", "Flyknit"]
 MODELS = [None, 350, 1, 90, 97]
 VERSIONS = [None, "V2", "2pto"]
-HEIGHTS = [None, "High", "Medium", "Low"]
+HEIGHTS = [None, "High", "Low"]
 COLLABORATIONS = [None, "off-white"]
 COLORS = [
     None, 'Beluga', 'Core Black Copper', 'Core Black Green', 'Core Black Red', 'Core Black White',
@@ -38,53 +40,70 @@ COLORS = [
 with open(model_path, 'rb') as file:
     clf = pickle.load(file)
 
+with open(scaler_path, 'rb') as file:
+    scaler = pickle.load(file)
+    
 def clean_dataset(data, TRAIN_COLS):
-    data = data.where(pd.notnull(data), None)
-    data.fillna(np.nan, inplace=True)
-
-    data_scaler = data.copy()
-    categorical_cols = ['Brand', 'Sub-brand', 'Product Line', 'Model', 'Version', 'Height', 'Collaboration', 'Color(s)']
     numerical_cols = ['Retail Price', 'Shoe Size']
-
-    data.drop(columns=["Buyer Region"], inplace=True, errors='ignore')
+    data[numerical_cols] = scaler.transform(data[numerical_cols])
+    
+    df_final = pd.DataFrame(columns=TRAIN_COLS)
 
     data['Order Date'] = pd.to_datetime(data['Order Date'])
     data['Release Date'] = pd.to_datetime(data['Release Date'])
 
-    data['Order Year'] = data['Order Date'].dt.year
-    data['Order Month'] = data['Order Date'].dt.month
-    data['Order Day'] = data['Order Date'].dt.day
-    data['Order Day of Week'] = data['Order Date'].dt.dayofweek
+    df_final['Order Date'] = data['Order Date']
+    df_final['Retail Price'] = data['Retail Price']
+    df_final['Release Date'] = data['Release Date']
+    df_final['Shoe Size'] = data['Shoe Size']
 
-    data['Release Year'] = data['Release Date'].dt.year
-    data['Release Month'] = data['Release Date'].dt.month
-    data['Release Day'] = data['Release Date'].dt.day
-    data['Release Day of Week'] = data['Release Date'].dt.dayofweek
+    df_final['Order Year'] = data['Order Date'].dt.year
+    df_final['Order Month'] = data['Order Date'].dt.month
+    df_final['Order Day'] = data['Order Date'].dt.day
+    df_final['Order Day of Week'] = data['Order Date'].dt.dayofweek
 
-    data['Order Date'] = data['Order Date'].astype(np.int64) // 10**9
-    data['Release Date'] = data['Release Date'].astype(np.int64) // 10**9
+    df_final['Release Year'] = data['Release Date'].dt.year
+    df_final['Release Month'] = data['Release Date'].dt.month
+    df_final['Release Day'] = data['Release Date'].dt.day
+    df_final['Release Day of Week'] = data['Release Date'].dt.dayofweek
 
-    data = pd.get_dummies(data, columns=categorical_cols, drop_first=True)
+    df_final['Order Date'] = df_final['Order Date'].astype(np.int64) // 10**9
+    df_final['Release Date'] = df_final['Release Date'].astype(np.int64) // 10**9
 
-    scaler = StandardScaler()
-    scaler.fit(data_scaler[numerical_cols])
-    data[numerical_cols] = scaler.transform(data[numerical_cols])
+    if f'Brand_{data["Brand"][0]}' in df_final.columns:
+        df_final[f'Brand_{data["Brand"][0]}'] = 1
+    if f'Sub-brand_{data["Sub-brand"][0]}' in df_final.columns:
+        df_final[f'Sub-brand_{data["Sub-brand"][0]}'] = 1
+    if f'Product Line_{data["Product Line"][0]}' in df_final.columns:
+        df_final[f'Product Line_{data["Product Line"][0]}'] = 1
+    if f'Model_{data["Model"][0]}' in df_final.columns:
+        df_final[f'Model_{data["Model"][0]}'] = 1
+    if f'Version_{data["Version"][0]}' in df_final.columns:
+        df_final[f'Version_{data["Version"][0]}'] = 1
+    if f'Height_{data["Height"][0]}' in df_final.columns:
+        df_final[f'Height_{data["Height"][0]}'] = 1
+    if 'Collaboration_Off White' in df_final.columns:
+        df_final['Collaboration_Off White'] = 1 if data['Collaboration'][0] == 'off-white' else 0
 
-    for col in TRAIN_COLS:
-        if col not in data.columns:
-            data[col] = 0
-    data = data[TRAIN_COLS]
+    if "Color(s)" in data.columns and data["Color(s)"].iloc[0]:
+        color_list = data["Color(s)"].iloc[0].split(', ')
+        
+        for color in color_list:
+            color_column = f'Color_{color}'
+            if color_column in df_final.columns:
+                df_final[color_column] = 1
 
-    return data
+    df_final = df_final.fillna(0)
 
+    return df_final
+    
 def predict_shoe_data(
-        retail_price, order_date, release_date, shoe_size, buyer_region, brand, sub_brand,
+        retail_price, order_date, release_date, shoe_size, brand, sub_brand,
         product_line, model, version, height, collaboration, color
 ):
     if not retail_price or not release_date or not shoe_size or not brand:
         return "Error: Por favor completa todos los campos obligatorios."
-
-    # Convierte la lista de colores seleccionados en una cadena unificada
+    
     color = ', '.join(color) if color else None
 
     data = {
@@ -92,7 +111,6 @@ def predict_shoe_data(
         "Order Date": [order_date],
         "Release Date": [release_date],
         "Shoe Size": [shoe_size],
-        "Buyer Region": [buyer_region],
         "Brand": [brand],
         "Sub-brand": [sub_brand],
         "Product Line": [product_line],
@@ -119,7 +137,6 @@ iface = gr.Interface(
         gr.Textbox(label="Order Date", value=str(datetime.today().date()), placeholder="YYYY-MM-DD"),
         gr.Textbox(label="Release Date", placeholder="YYYY-MM-DD"),
         gr.Number(label="Shoe Size"),
-        gr.Dropdown(label="Buyer Region", choices=[None] + USA_STATES, multiselect=False),
         gr.Dropdown(label="Brand", choices=BRANDS),
         gr.Dropdown(label="Sub-brand", choices=SUB_BRANDS),
         gr.Dropdown(label="Product Line", choices=PRODUCT_LINES),
